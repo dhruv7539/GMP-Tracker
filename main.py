bfc073fd9e09e95c422e7bf0726a85342a0d8c59
dhruv7539 <dhruvbhanderi7@gmail.com>
2024-10-20 12:32:29 -0700

    Updated numpy version and set Python to 3.9

--- main.py
+++ main.py
@@ -163,4 +163,5 @@ def home():
     return render_template_string(html_template, table=ipo_html_table)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
